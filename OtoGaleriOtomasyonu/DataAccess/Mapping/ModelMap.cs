7299d3f1c3973c2009e7d21cad3832540f9af90e
Volkan Akdere <vvoollkkaann@hotmail.com.tr>
2021-05-04 23:53:07 +0300

    Veri tabanı nesneleri Mapeandi

---
+++ OtoGaleriOtomasyonu/DataAccess/Mapping/ModelMap.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Domains;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Mapping
+{
+    public class ModelMap : IEntityTypeConfiguration<Model>
+    {
+        public void Configure(EntityTypeBuilder<Model> builder)
+        {
+            builder.ToTable(@"Modeller", @"dbo");
+            builder.HasKey(a => a.Id);
+
+        }
+    }
+}
---
+++ OtoGaleriOtomasyonu/DataAccess/Mapping/YakitTurMap.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Domains;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Mapping
+{
+    class YakitTurMap : IEntityTypeConfiguration<YakitTur>
+    {
+        public void Configure(EntityTypeBuilder<YakitTur> builder)
+        {
+            builder.ToTable(@"YakitTurleri", @"dbo");
+            builder.HasKey(a => a.Id);
+        }
+    }
+}
